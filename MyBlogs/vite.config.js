2e0c8db073560bd0d0d17aa22aada2886d2d940e
Amit Thakur <amitthakur@Amits-MacBook-Pro.local>
2026-03-15 22:43:47 +0530

    fix: simplify vite config

--- MyBlogs/vite.config.js
+++ MyBlogs/vite.config.js
@@ -2,19 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
   server: {
     proxy: {
-      "/api" : {
-         target: env.VITE_API_TARGET,
+      "/api": {
+        target: "https://blog-api-74l6.onrender.com",
         secure: false,
         changeOrigin: true,
       }
     }
-
   }
-
 })
